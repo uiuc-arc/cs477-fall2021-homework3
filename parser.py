25355e1bf1c117decea5831731328288318af664
vimuth <vimuth@gmail.com>
2021-11-03 11:20:35 -0500

    Adding the answer

--- parser.py
+++ parser.py
@@ -10,14 +10,6 @@ import networkx as nx
 
 import operator
 
-
-# This class holds the data for a Control Flow Graph node
-# content - the code for the node
-# text    - is a textual representation
-# isSplit - denotes whether the CFG node is splitting the execution (start of a loop, conditional)
-# bbid    - Integer id for the node
-# nextblock - If the node is not a split node this will contain a link to the next node in the graph
-# trueCase, falseCase - If the node is a split node these will point to the two possible next nodes
 class CFGNode:
     def __init__(self, *args):
         self.content = args[0]
@@ -49,17 +41,8 @@ class CFG:
         finalNode.setNextBlock(self.endNode)
         self.maxBBId = finalid+1
 
-
-    # For each statement we will define here how to add the statement to a CFG
-    # The inputs to this function are:
-    # 1. the current statment being processed
-    # 2. the previous node - so that we can point it to the current node
-    # 3. Current highest id for the cfg nodes
-    #
-    # The function returns the updated bbid, start of the newly added section to the CFG and the end node of the new section.
+    
     def processSingleStatement(statement, prevNode, bbid):
-        # For statements that does not impact control flow we create a new object
-        # We then set the prevNode's next block as the current  block
         if (isinstance(statement, pointersParser.AssignContext) or
             isinstance(statement, pointersParser.AllocContext) or
             isinstance(statement, pointersParser.SkipContext)):
@@ -67,8 +50,7 @@ class CFG:
             if prevNode:
                 prevNode.setNextBlock(newBlock)
             return bbid+1, newBlock, newBlock
-
-        # If the statement is a if condition, 
+        
         if isinstance(statement, pointersParser.IfContext):
             newBlock = CFGNode(None, "IF: [{}]".format(statement.cond.getText()), True, bbid + 1)
             bbid, ifbranch, endNode1 =  CFG.buildCFG(statement.ifs, None, bbid + 1)
@@ -131,18 +113,11 @@ class CFG:
             if node.isSplit:
                 CFG.drawCFGHelper(node.trueCase, node.bbid, G)
                 CFG.drawCFGHelper(node.falseCase, node.bbid, G)
-                G.add_edge(nodeFormatStr.format(node.bbid, node.text),
-                           nodeFormatStr.format(node.trueCase.bbid,
-                                                node.trueCase.text))
-                G.add_edge(nodeFormatStr.format(node.bbid, node.text),
-                           nodeFormatStr.format(node.falseCase.bbid,
-                                                node.falseCase.text))
+                G.add_edge(nodeFormatStr.format(node.bbid, node.text), nodeFormatStr.format(node.trueCase.bbid, node.trueCase.text))
+                G.add_edge(nodeFormatStr.format(node.bbid, node.text), nodeFormatStr.format(node.falseCase.bbid, node.falseCase.text))
             else:
                 if node.nextblock: 
-                    G.add_edge(nodeFormatStr.format(node.bbid,
-                                                    node.text),
-                               nodeFormatStr.format(node.nextblock.bbid,
-                                                    node.nextblock.text))
+                    G.add_edge(nodeFormatStr.format(node.bbid, node.text), nodeFormatStr.format(node.nextblock.bbid, node.nextblock.text))
             if node.nextblock and node.nextblock.bbid <= node.bbid:
                 break
             node = node.nextblock
@@ -168,21 +143,13 @@ class AbstractInterpretation():
         self.ast = ast        
         self.cfg = cfg
         self.absDomain = absDomain
-        # The statemap is initialized to have an abstract state for each CFG node. 
         self.stateMap = self.getInitialStateMap()
-        # A list of statements are created. The analysis applies the
-        # transfer functions along this order.
         self.statementList = cfg.getList()
 
-    
     def getInitialStateMap(self):
-        # Following code goes through the code and identifies all the variables used in the program
         variableExplorer = getVarSet()
         walker = ParseTreeWalker()
         walker.walk(variableExplorer, self.ast)
-
-        # Then, for each node in the CFG, following code creates a map
-        # from each variable to the bottom element of the abstract domain
         stateMap = {}
         for i in range(self.cfg.maxBBId+1):
             stateMap[i] = dict.fromkeys(variableExplorer.varset, self.absDomain.bottomElement)        
@@ -195,50 +162,30 @@ class AbstractInterpretation():
     def run(self):
         self.runHelper(self.statementList.copy())
 
-    # This function performs the abstract interpretation
-    # The input to the function is a list of cfg nodes
     def runHelper(self, nodeList):
-        # If the list is empty we are done
         if len(nodeList) == 0:
-            return
-
-        # We pop the first node of the list and applies the transformer function on the node
+            return 
         node = nodeList.pop(0)
-
-        # If the node is not a split node we can apply the transfer function
-        # If the transfer function changes the abstract state for the next node we will merge the two states
         if not node.isSplit:
             nextBlock = node.nextblock
             if nextBlock:
                 myState = self.stateMap[node.bbid]
                 oldState = self.stateMap[nextBlock.bbid].copy()
-
-                # Applying the transfer function based on the abstract domain
-                newState = self.absDomain.statementTransfer(nextBlock, myState)
+                newState = self.absDomain.statementTransfer(nextBlock, myState, oldState)
                 self.stateMap[nextBlock.bbid] = self.absDomain.merge(oldState, newState)
-
                 if self.absDomain.isEqual(oldState, newState):
-                    # If the state did not change we will continue the analysis on the remaining node
                     return self.runHelper(nodeList)
                 else:
-                    # if the state changed we have not reached a fixed point
-                    # In this case we will re add the statement to the list
                     return self.runHelper(nodeList + [nextBlock])
             else:
-                # If this is the last node,, do nothing
                 return self.runHelper(nodeList)
         else:
-            # To handle split nodes we will apply the tranfer function to boith branches
-            # We will change the state for the branches if they change due to the transfer function
             myState = self.stateMap[node.bbid]
             oldStateT = self.stateMap[node.trueCase.bbid].copy()            
-            newStateT = self.absDomain.statementTransfer(node.trueCase, myState,)
+            newStateT = self.absDomain.statementTransfer(node.trueCase, myState, oldStateT)
             oldStateF = self.stateMap[node.falseCase.bbid].copy()
-            newStateF = self.absDomain.statementTransfer(node.falseCase, myState)
+            newStateF = self.absDomain.statementTransfer(node.falseCase, myState, oldStateF)
 
-            # If the states chaged we have not reached a fixed point.
-            # Based on whether a single path or both paths changed, we
-            # will add the statments back to the analysis
             if self.absDomain.isEqual(oldStateT, newStateT):
                 if self.absDomain.isEqual(oldStateF, newStateF):
                     return self.runHelper(nodeList)
@@ -263,47 +210,65 @@ class PointersDomain():
     # We have already defined the bottom element to be the empty set and the top element to be a set with ['null']
     # Elements of the abstractDomain are sets of object allocation sites
     def lub(a, b):
-        return []
+        if a == PointersDomain.topElement or b == PointersDomain.topElement:
+            return PointersDomain.topElement
+        return a.union(b).copy()
 
     # Checks if two abstract states are the same
     # Remember that the abstract states map each variable to a element in the abstract domain
     def isEqual(state1, state2):
+        for var in state1:
+            if var not in state2:
+                return False
+            if state1[var] != state2[var]:
+                return False
         return True
 
-    # This is the main transfer function that need to be implemented.
+    # This is the main tranfer function that need to be implemented.
     # For each type of statement define how the currentState get transformed and return the updated state.
-    def statementTransfer(block, currentState):
+    def statementTransfer(block, currentState, nextAbstractState):
+        newState = currentState.copy()
         if isinstance(block.content, pointersParser.SkipContext):
             # what needs to happen if it is a skip statement
-            return []
+            return currentState.copy()
         elif isinstance(block.content, pointersParser.AssignContext):
-            # To access the name of the assigned variable you can use block.content.variable(0).getText()
+            var_a = block.content.variable(0).getText()
             if not isinstance(block.content.variable(1), pointersParser.NullvarContext):
+                var = block.content.variable(1).getText()
                 # what need to be done if the variable is assigned another variable
-                return []
+                newState[var_a] = currentState[var]
+                return newState
             else:
                 # what need to be done if the variable is assigned null                
-                return []
+                newState[var_a] = PointersDomain.topElement
+                return newState
         elif isinstance(block.content, pointersParser.AllocContext):
             # how to handle the newObject statement
             # use block.bbid to access the block id of the current CFG node
             # use block.content.variable().getText() to access the variable being assigned
-            pass
+            var = block.content.variable().getText()
+            if var not in currentState:
+                newState[var] = PointersDomain.bottomElement
+            #newState[var] = newState[var].union({block.bbid})
+            newState[var] = {block.bbid}
+            return newState
         else:
             # For split nodes in the CFG we will be adding join nodes. Those nodes do not change the state
-            return currentState
+            return currentState.copy()
 
-    # how do we merge two abstract states together
+    # how do we merge two abstract states togeter
     # Remember that the abstract states map each variable to a element in the abstract domain
     # hint use the PointersDomain.lub function
     def merge(abstractState1, abstractState2):
-        # Replace the return statement. This is currently here to prevent the program crashing. 
-        return abstractState1
+        res = {}
+        for key in abstractState1.keys():
+            res[key] = PointersDomain.lub(abstractState1[key], abstractState2[key])
+        return res
+
 
 if __name__ == '__main__':
     input_file = sys.argv[1]
-
-    # Reading the input program and building a CFG
+    
     program_str = open(input_file).read()
     input_stream = InputStream(program_str)
     lexer = pointersLexer(input_stream)
@@ -319,7 +284,6 @@ if __name__ == '__main__':
     CFG.drawCFG(cfg.startNode)
     print('--------------')
 
-    # Applying Abstract Interpretation
     absInterp = AbstractInterpretation(ast, cfg, PointersDomain)
     absInterp.run()
     absInterp.printAbsState()
